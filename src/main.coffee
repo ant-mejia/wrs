227f4cc32a81f256b9591ff91ad2dfabe88696a9
Anthony Mejia <git@antmejia.com>
2017-10-13 20:28:12 -0400

    Updated Blimp

--- src/main.coffee
+++ src/main.coffee
@@ -4,6 +4,8 @@ require 'velocity-animate'
 Highcharts = require('highcharts');
 
 $ ->
+  if $(window).scrollTop() > $('.c-jumbotron').height()
+    $(".c-header").addClass 'opaque'
   $(window).on 'scroll', () ->
     buffer = $('.c-jumbotron').height() - $('.c-header').height()
     if $(window).scrollTop() > buffer
@@ -20,7 +22,7 @@ $ ->
       bottom: 0,
       left: 0
 
-  sr.reveal "#main-section *:not(.o-container_section):not(.o-grid)"
+  sr.reveal "#main-section *:not(.o-container_section):not(.o-grid):not(.c-blimp):not(.c-blimp_slideshow):not(.c-blimp_slideshow-slide):not(.nsr)"
   # sr.reveal "div[class$='-wrapper'],div[class*='-wrapper ']"
   sr.reveal "footer *"
 
@@ -41,6 +43,67 @@ $ ->
     else
       console.log 'no'
 
+  $(".c-blimp_menu-item").on "click", () ->
+    console.log "success!"
+    console.log $(@).index()
+    blimpContainer = $(@).parent().parent().parent()
+    blimpSlideshow = blimpContainer.find(".c-blimp_slideshow")
+    if not $(@).is "[data-blimp='active']"
+      blimpSlideshow.children().attr "data-blimp", ""
+      blimpSlideshow.children().eq($(@).index()).attr "data-blimp", "active"
+      if $(@).is(":first-child")
+        blimpSlideshow.children().last().attr "data-blimp", "prev"
+        blimpSlideshow.children().eq($(@).next().index()).attr "data-blimp", "next"
+      else if $(@).is(":last-child")
+        console.log $(@).prev().index()
+        blimpSlideshow.children().eq($(@).prev().index()).attr "data-blimp", "prev"
+        blimpSlideshow.children().first().attr "data-blimp", "next"
+      else
+        blimpSlideshow.children().eq($(@).prev().index()).attr "data-blimp", "prev"
+        blimpSlideshow.children().eq($(@).next().index()).attr "data-blimp", "next"
+        console.log "normal"
+
+  $(".c-blimp_menu-control").on "click", ->
+    controlItem = $ @
+    controlIndex = controlItem.index()
+    slideshow = controlItem.parents().filter(".c-blimp_container").find ".c-blimp_slideshow"
+    slides = slideshow.children()
+    if slides.is "[data-blimp='active']"
+      activeSlide = slides.filter "[data-blimp='active']"
+      activeIndex = activeSlide.index()
+      slides.attr "data-blimp", ""
+      console.log activeIndex
+      if controlIndex == 0
+        if activeSlide.is(":first-child")
+          slides.last().prev().attr "data-blimp", "prev"
+          slides.last().attr "data-blimp", "active"
+          slides.first().attr "data-blimp", "next"
+        else if activeIndex == 1
+          console.log 'booya'
+          slides.last().attr "data-blimp", "prev"
+          activeSlide.prev().attr "data-blimp", "active"
+          activeSlide.attr "data-blimp", "next"
+        else
+          activeSlide.prev().prev().attr "data-blimp", "prev"
+          activeSlide.prev().attr "data-blimp", "active"
+          activeSlide.attr "data-blimp", "next"
+      else
+        if activeSlide.is(":last-child")
+          activeSlide.attr "data-blimp", "prev"
+          slides.first().attr "data-blimp", "active"
+          slides.first().next().attr "data-blimp", "next"
+        else if activeIndex == slides.length - 2
+          # body...
+          console.log "booya"
+          activeSlide.attr "data-blimp", "prev"
+          activeSlide.next().attr "data-blimp", "active"
+          slides.first().attr "data-blimp", "next"
+
+        else
+          activeSlide.attr "data-blimp", "prev"
+          activeSlide.next().attr "data-blimp", "active"
+          activeSlide.next().next().attr "data-blimp", "next"
+
   $('a[href*="#"]').on "click", (e) ->
       e.preventDefault();
       e.stopPropagation();
@@ -48,7 +111,7 @@ $ ->
 
       $(target).velocity 'scroll', {
         duration: 700,
-        offset: -40,
+        offset: -100,
         easing: 'easeOutCubic'
       }
   Highcharts.chart 'container',
--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ require('velocity-animate');
 Highcharts = require('highcharts');
 
 $(function() {
+  if ($(window).scrollTop() > $('.c-jumbotron').height()) {
+    $(".c-header").addClass('opaque');
+  }
   $(window).on('scroll', function() {
     var buffer;
     buffer = $('.c-jumbotron').height() - $('.c-header').height();
@@ -26,7 +29,7 @@ $(function() {
       left: 0
     }
   });
-  sr.reveal("#main-section *:not(.o-container_section):not(.o-grid)");
+  sr.reveal("#main-section *:not(.o-container_section):not(.o-grid):not(.c-blimp):not(.c-blimp_slideshow):not(.c-blimp_slideshow-slide):not(.nsr)");
   sr.reveal("footer *");
   $(".c-header_nav-button").on("click", function(event) {
     var eventTarget;
@@ -46,6 +49,73 @@ $(function() {
       return console.log('no');
     }
   });
+  $(".c-blimp_menu-item").on("click", function() {
+    var blimpContainer, blimpSlideshow;
+    console.log("success!");
+    console.log($(this).index());
+    blimpContainer = $(this).parent().parent().parent();
+    blimpSlideshow = blimpContainer.find(".c-blimp_slideshow");
+    if (!$(this).is("[data-blimp='active']")) {
+      blimpSlideshow.children().attr("data-blimp", "");
+      blimpSlideshow.children().eq($(this).index()).attr("data-blimp", "active");
+      if ($(this).is(":first-child")) {
+        blimpSlideshow.children().last().attr("data-blimp", "prev");
+        return blimpSlideshow.children().eq($(this).next().index()).attr("data-blimp", "next");
+      } else if ($(this).is(":last-child")) {
+        console.log($(this).prev().index());
+        blimpSlideshow.children().eq($(this).prev().index()).attr("data-blimp", "prev");
+        return blimpSlideshow.children().first().attr("data-blimp", "next");
+      } else {
+        blimpSlideshow.children().eq($(this).prev().index()).attr("data-blimp", "prev");
+        blimpSlideshow.children().eq($(this).next().index()).attr("data-blimp", "next");
+        return console.log("normal");
+      }
+    }
+  });
+  $(".c-blimp_menu-control").on("click", function() {
+    var activeIndex, activeSlide, controlIndex, controlItem, slides, slideshow;
+    controlItem = $(this);
+    controlIndex = controlItem.index();
+    slideshow = controlItem.parents().filter(".c-blimp_container").find(".c-blimp_slideshow");
+    slides = slideshow.children();
+    if (slides.is("[data-blimp='active']")) {
+      activeSlide = slides.filter("[data-blimp='active']");
+      activeIndex = activeSlide.index();
+      slides.attr("data-blimp", "");
+      console.log(activeIndex);
+      if (controlIndex === 0) {
+        if (activeSlide.is(":first-child")) {
+          slides.last().prev().attr("data-blimp", "prev");
+          slides.last().attr("data-blimp", "active");
+          return slides.first().attr("data-blimp", "next");
+        } else if (activeIndex === 1) {
+          console.log('booya');
+          slides.last().attr("data-blimp", "prev");
+          activeSlide.prev().attr("data-blimp", "active");
+          return activeSlide.attr("data-blimp", "next");
+        } else {
+          activeSlide.prev().prev().attr("data-blimp", "prev");
+          activeSlide.prev().attr("data-blimp", "active");
+          return activeSlide.attr("data-blimp", "next");
+        }
+      } else {
+        if (activeSlide.is(":last-child")) {
+          activeSlide.attr("data-blimp", "prev");
+          slides.first().attr("data-blimp", "active");
+          return slides.first().next().attr("data-blimp", "next");
+        } else if (activeIndex === slides.length - 2) {
+          console.log("booya");
+          activeSlide.attr("data-blimp", "prev");
+          activeSlide.next().attr("data-blimp", "active");
+          return slides.first().attr("data-blimp", "next");
+        } else {
+          activeSlide.attr("data-blimp", "prev");
+          activeSlide.next().attr("data-blimp", "active");
+          return activeSlide.next().next().attr("data-blimp", "next");
+        }
+      }
+    }
+  });
   $('a[href*="#"]').on("click", function(e) {
     var target;
     e.preventDefault();
@@ -53,7 +123,7 @@ $(function() {
     target = $(this).attr('href');
     return $(target).velocity('scroll', {
       duration: 700,
-      offset: -40,
+      offset: -100,
       easing: 'easeOutCubic'
     });
   });
